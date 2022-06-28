99cad8a57c2e49721336c14a8b5d2eabd77e518e
Josh <joshnk027@gmail.com>
2022-06-28 17:36:04 +1200

    Games

---
+++ 3DDungeonCrawler/Assets/Scripts/GameController.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(MazeConstructor))]
+
+public class GameController : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject monsterPrefab;
+
+    private MazeConstructor constructor;
+    [SerializeField] private int rows;
+    [SerializeField] private int cols;
+
+    private AIController aIController;
+
+    void Awake()
+    {
+        constructor = GetComponent<MazeConstructor>();
+        aIController = GetComponent<AIController>();
+    }
+
+    private GameObject CreatePlayer()
+    {
+        Vector3 playerStartPosition = new Vector3(constructor.hallWidth, 1, constructor.hallWidth);
+        GameObject player = Instantiate(playerPrefab, playerStartPosition, Quaternion.identity);
+        player.tag = "Generated";
+
+        return player;
+    }
+
+    private GameObject CreateMonster()
+    {
+        Vector3 monsterPosition = new Vector3(constructor.goalCol * constructor.hallWidth, 0f, constructor.goalRow * constructor.hallWidth);
+        GameObject monster = Instantiate(monsterPrefab, monsterPosition, Quaternion.identity);
+        monster.tag = "Generated";
+
+        return monster;
+    }
+
+
+    void Start()
+    {
+        constructor.GenerateNewMaze(rows, cols, OnTreasureTrigger);
+
+        aIController.Graph = constructor.graph;
+        aIController.Player = CreatePlayer();
+        aIController.Monster = CreateMonster();
+        aIController.HallWidth = constructor.hallWidth;
+        aIController.StartAI();
+    }
+
+    private void OnTreasureTrigger(GameObject trigger, GameObject other)
+    {
+        Debug.Log("You Won!");
+        aIController.StopAI();
+    }
+
+}
